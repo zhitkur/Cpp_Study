1e8095aba7ff17a3de01b5273d84467a7f426598
zhitkur <zhitkur09@gmail.com>
2020-10-25 15:38:44 +0900

    nullptr

---
+++ nullptr.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int const_x(const char *p, char x) {
+    // p[]에 존재하는 x와 동일한 값의 수를 센다.
+    // p는 0으로 끝나는 char 배열을 가리킨다고 가정(그렇지 않으면 아무것도 가리키지 않는다.)
+    if (p == nullptr)
+        return 0;
+    int count = 0;
+    
+    /*
+    for (; *p != 0; ++p)
+        if (*p == x)
+            ++count;
+    */
+    while (*p) {
+        if (*p == x) {
+            ++count;
+            cout << *p << '\n';
+        }
+        ++p;
+    }
+    return count;
+}
+
+
+int main(void) {
+    printf("%d\n", const_x("AHCDEHGHIJKLNM", 'H'));
+
+    return 0;
+
+}
